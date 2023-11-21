3bf464701686bef48a5874add72a913957b5993f
TaliorTheRogue <donribeiro45@gmail.com>
2023-11-21 11:26:11 +0100

    postit creation created with style

--- index.php
+++ index.php
@@ -7,5 +7,58 @@ catch (Exception $e)
 {
 die('Erreur : ' . $e->getMessage());
 }
+$query = 'SELECT * FROM postits';
+$response = $bdd->query($query);
 
-require "./layout.php";
+$datas = $response->fetchAll();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Memento</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Shadows+Into+Light&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <h1>Memento</h1>
+            <div class="userlinks">
+                <a href="#" title="Login">Login</a>
+                <a href="#" title="Register">Register</a>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div id="title_memento">
+            <h2>Memento</h2>
+            <a href="postit_creator.php">Add Postit</a>
+            <div id="postit_grid">
+                <?php foreach($datas as $data) {?>
+                <article>
+                    <div class="postit_header">
+                        <h3><?= $data['title'] ?></h3>
+                        <button class="postit_delete">X</button>
+                    </div>
+                    <p><?= $data['content'] ?></p>
+                    <div class="postit_footer">
+                        <p><?= $data['date'] ?></p>
+                    </div>
+                </article>
+                <?php } ?>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+    
+</body>
+
+</html>
---
+++ postit_creator.php
@@ -0,0 +1,69 @@
+<?php
+    try
+    {
+    $bdd = new PDO('mysql:host=localhost;dbname=memento;charset=utf8', 'root', '');
+    }
+    catch (Exception $e)
+    {
+    die('Erreur : ' . $e->getMessage());
+    }
+
+    if($_SERVER['REQUEST_METHOD']=== 'POST'){
+        $req = $bdd->prepare('INSERT INTO postits(title, content, date) VALUES(:title, :content, :date)');
+        $req->execute(array(
+            ':title' => $_POST['title'],
+            ':content' => $_POST['content'],
+            ':date' => $_POST['date'],
+        ));
+        if($req->execute(array(
+            ':title' => $_POST['title'],
+            ':content' => $_POST['content'],
+            ':date' => $_POST['date'],
+            ))){
+        echo 'Votre postit a bien été créé !';
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Memento</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/postit_creator.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Shadows+Into+Light&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+            <nav>
+                <h1>Memento</h1>
+                <div class="userlinks">
+                    <a href="index.php" title="Home">Home</a>
+                    <a href="#" title="Login">Login</a>
+                    <a href="#" title="Register">Register</a>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <form method="post" action="postit_creator.php">
+                <label for="title">Titre:</label>
+                <input name="title" id="title" type="text" placeholder="Your title">
+
+                <label for="content">Content:</label>
+                <textarea name="content" id="content" type="text" placeholder="Write here"></textarea>
+
+                <label for="date">Date:</label>
+                <input name="date" id="date" type="date" placeholder="Date">
+
+                <button type="submit">Create postit</button>
+            </form>
+        </main>
+        <footer>
+
+        </footer>
+</body>
+</html>
